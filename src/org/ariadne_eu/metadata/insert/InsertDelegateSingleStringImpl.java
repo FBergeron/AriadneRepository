5d2a9288039f44bda2eaeae315569d9d50b3fa84
gap_83 <gap_83@f3d16e92-8d2b-0410-a261-e2b3ece8207d>
2009-03-10 14:10:35 +0000

    git-svn-id: https://ariadnekps.svn.sourceforge.net/svnroot/ariadnekps/AriadneRepository@306 f3d16e92-8d2b-0410-a261-e2b3ece8207d

--- src/org/ariadne_eu/metadata/insert/InsertDelegateSingleStringImpl.java
+++ src/org/ariadne_eu/metadata/insert/InsertDelegateSingleStringImpl.java
@@ -11,6 +11,8 @@ import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.Term;
+import org.ariadne_eu.utils.config.ConfigManager;
+import org.ariadne_eu.utils.config.RepositoryConstants;
 import org.ariadne_eu.utils.lucene.indexer.MACEEnrichment;
 import org.eun.lucene.core.indexer.document.DocumentHandler;
 import org.eun.lucene.core.indexer.document.DocumentHandlerException;
@@ -35,16 +37,20 @@ public class InsertDelegateSingleStringImpl implements IndexInserterDelegate {
             if (metadata.startsWith("<?")) {
                 insertMetadata = metadata.substring(metadata.indexOf("?>")+2);
             }
-            
-            String exml = MACEEnrichment.enrichWClassification(insertMetadata);
 			
 			doc = handler.getDocument(new ByteArrayInputStream(metadata.getBytes("UTF-8")));
 			
 			doc.add(new Field("key", key, Field.Store.YES, Field.Index.UN_TOKENIZED ));
 			doc.add(new Field("date.insert", DateTools.dateToString(new Date(), DateTools.Resolution.MILLISECOND), Field.Store.YES, Field.Index.UN_TOKENIZED));
             doc.add(new Field("lom", insertMetadata, Field.Store.YES, Field.Index.UN_TOKENIZED, Field.TermVector.WITH_POSITIONS_OFFSETS));
-            doc.add(new Field("maceenrichedlom", exml, Field.Store.YES, Field.Index.UN_TOKENIZED, Field.TermVector.WITH_POSITIONS_OFFSETS));
+            
             doc.add(new Field("lom.solr", "all", Field.Store.YES, Field.Index.UN_TOKENIZED, Field.TermVector.WITH_POSITIONS_OFFSETS));
+            
+            String luceneHandler = ConfigManager.getProperty(RepositoryConstants.MD_LUCENE_HANDLER);
+            if (luceneHandler.equalsIgnoreCase("org.ariadne_eu.metadata.insert.lucene.document.MACELOMHandler")) {
+            	String exml = MACEEnrichment.enrichWClassification(insertMetadata);
+            	doc.add(new Field("maceenrichedlom", exml, Field.Store.YES, Field.Index.UN_TOKENIZED, Field.TermVector.WITH_POSITIONS_OFFSETS));
+            }
 
 //            Term term = new Term("key", key);
 //            writer.deleteDocuments(term);
