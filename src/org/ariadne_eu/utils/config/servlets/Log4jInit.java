1c29d359b106cf794970515dd343a3cfbbbe9dec
bramv_cs <bramv_cs@f3d16e92-8d2b-0410-a261-e2b3ece8207d>
2010-02-10 21:21:40 +0000

    Log4jInit.reloadLive
    git-svn-id: https://ariadnekps.svn.sourceforge.net/svnroot/ariadnekps/AriadneRepository@479 f3d16e92-8d2b-0410-a261-e2b3ece8207d

--- src/org/ariadne_eu/utils/config/servlets/Log4jInit.java
+++ src/org/ariadne_eu/utils/config/servlets/Log4jInit.java
@@ -17,31 +17,48 @@ public class Log4jInit extends HttpServlet {
 	protected static String prefix = "";
 	protected static String file = "";
 
-  public void init() {
-	  reloadLogging();
-  }
+	public void init() {
+		reloadLogging();
+	}
 
-  public void reloadLogging(){
-	    logDir = PropertiesManager.getInstance().getProperty(RepositoryConstants.REPO_LOG4J_DIR);
-	    logFile = PropertiesManager.getInstance().getProperty(RepositoryConstants.REPO_LOG4J_FILENAME);
-	    if(!logDir.equals("")){
-	    	System.setProperty("logdir", logDir);
-	    }
-	    if(!logFile.equals("")){
-	    	System.setProperty("logfile", logFile);
-	    }
-	    else {
-	    	System.setProperty("logfile", "default_log");
-	    }
-	    prefix =  getServletContext().getRealPath("install");
-	    file = getInitParameter("log4j-init-file");
-	    //  if the log4j-init-file is not set, then no point in trying
-	    if(file != null) {
-	        PropertyConfigurator.configure(prefix + File.separator + file);
-	      }
-  }
-  
-  public
-  void doGet(HttpServletRequest req, HttpServletResponse res) {
-  }
+	public void reloadLogging(){
+		logDir = PropertiesManager.getInstance().getProperty(RepositoryConstants.REPO_LOG4J_DIR);
+		logFile = PropertiesManager.getInstance().getProperty(RepositoryConstants.REPO_LOG4J_FILENAME);
+		if(!logDir.equals("")){
+			System.setProperty("logdir", logDir);
+		}
+		if(!logFile.equals("")){
+			System.setProperty("logfile", logFile);
+		}
+		else {
+			System.setProperty("logfile", "default_log");
+		}
+		prefix =  getServletContext().getRealPath("install");
+		file = getInitParameter("log4j-init-file");
+		//  if the log4j-init-file is not set, then no point in trying
+		if(file != null) {
+			PropertyConfigurator.configure(prefix + File.separator + file);
+		}
+	}
+
+	public static void reloadLive() {
+		logDir = PropertiesManager.getInstance().getProperty(RepositoryConstants.REPO_LOG4J_DIR);
+		logFile = PropertiesManager.getInstance().getProperty(RepositoryConstants.REPO_LOG4J_FILENAME);
+		if (!logDir.equals("")) {
+			System.setProperty("logdir", logDir);
+		}
+		if (!logFile.equals("")) {
+			System.setProperty("logfile", logFile);
+		}else {
+			System.setProperty("logfile", "default_log");
+		}
+		// if the log4j-init-file is not set, then no point in trying
+		if (file != null) {
+			PropertyConfigurator.configure(prefix + File.separator + file);
+		}
+	}
+
+	public
+	void doGet(HttpServletRequest req, HttpServletResponse res) {
+	}
 }
